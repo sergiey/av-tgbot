894259519244069b03231e172075a6dd4fc37ba1
Sergei Melnikov <melnikhov@gmail.com>
2023-10-09 16:24:45 +0300

    Add program file.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using av_tgbot;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+var token = config["Token"];
+
+using CancellationTokenSource cts = new();
+var bot = new Bot(token, cts.Token);
+
+await bot.Run();
+while(true) {
+    string? o = Console.ReadLine();
+    if (o?.ToLower() == "exit" || o?.ToLower() == "quit")
+    {
+        cts.Cancel();
+        return;
+    }
+}
