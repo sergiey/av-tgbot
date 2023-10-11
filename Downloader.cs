0f37ae663a81f846554da2616ea1411e27027c22
Sergei Melnikov <melnikhov@gmail.com>
2023-10-12 00:31:20 +0300

    Rebuild downloader class.

--- Downloader.cs
+++ Downloader.cs
@@ -1,5 +1,5 @@
-using YoutubeDLSharp;
 using File = System.IO.File;
+using YoutubeDLSharp;
 
 namespace av_tgbot;
 
@@ -7,8 +7,10 @@ public class Downloader
 {
     private readonly string _options =
         "-f \"bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best\"";
+    private readonly float _maxVideoDuration = 180;
+    private string? _filePath;
 
-    public async Task<Stream> GetFileStream(string link)
+    public async Task<DownloadStatus> Download(string link)
     {
         var ytdl = new YoutubeDL
         {
@@ -17,7 +19,36 @@ public class Downloader
             OutputFolder = "/Users/sergeymelnikov/code/av-tgbot/downloads"
         };
 
-        var filePath = await ytdl.RunVideoDownload(link, _options);
-        return File.Open(filePath.Data, FileMode.Open);
+        var dataFetch = await ytdl.RunVideoDataFetch(link);
+        var videoDuration= dataFetch.Data.Duration;
+
+        if(videoDuration > _maxVideoDuration)
+            return new DownloadStatus(
+                false,
+                "Video is too long. Should be no more that 3 minutes."
+            );
+
+        var result = await ytdl.RunVideoDownload(link, _options);
+
+        if(result.Success)
+        {
+            _filePath = result.Data;
+            return new DownloadStatus(true, "OK");
+        }
+        
+        return new DownloadStatus(
+            false,
+            $"Error downloading video from link: {link}"
+        );
+    }
+
+    public Stream GetFileStream()
+    {
+        return File.Open(_filePath, FileMode.Open);
+    }
+
+    public void CleanUp()
+    {
+        File.Delete(_filePath);
     }
 }
